5cbd55616e9f5682dba0838afd31c0f413dfa28a
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-09-03 14:11:24 -0700

    Tests and implementation for df(4) and planetary_type(roll)

--- apps/mindjammer_world/lib/mindjammer_world/application.ex
+++ apps/mindjammer_world/lib/mindjammer_world/application.ex
@@ -9,12 +9,12 @@ defmodule MindjammerWorld.Application do
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: MindjammerWorld.Worker.start_link(arg)
-      # {MindjammerWorld.Worker, arg},
+      {MindjammerWorld.Worker, name: MindjammerWorld.Worker},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MindjammerWorld.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = Supervisor.start_link(children, opts)
   end
 end
---
+++ apps/mindjammer_world/lib/mindjammer_world/worker.ex
@@ -0,0 +1,42 @@
+defmodule MindjammerWorld.Worker do
+  @moduledoc false
+  use GenServer
+
+  # API
+
+  def df(dice_count) do
+    Enum.reduce((0..dice_count), 0, fn(_, acc) -> :rand.uniform(3) - 2 + acc end)
+  end
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def planetary_type(roll) do
+    GenServer.call(__MODULE__, {:planetary_type, roll})
+  end
+
+  # SERVER
+
+  def init(:ok), do: {:ok, %{}}
+
+  def handle_call({:planetary_type, roll}, _from, state) do
+    {:reply, do_planetary_type(roll), state}
+  end
+
+  def handle_info(_msg, state), do: {:noreply, state}
+
+  def terminate(_reason, _state), do: :ok
+
+  def code_change(_old_vsn, _state, _extra) do
+    {:error, :not_implemented}
+  end
+
+  ### PRIVATE FUNCTIONS
+
+  defp do_planetary_type(-4), do: {:non_garden_world, -4}
+  defp do_planetary_type(roll) when roll < -1, do: {:marginal_garden_world, -2}
+  defp do_planetary_type(roll) when roll < 2,  do: {:inferior_garden_world, 0}
+  defp do_planetary_type(roll) when roll < 4,  do: {:standard_garden_world, 2}
+  defp do_planetary_type(4),  do: {:superior_garden_world, 4}
+end
