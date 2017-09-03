70fd8daae0ab8aba819388fdc4658a5ef49c04f2
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-09-03 12:58:02 -0700

    Add tests for world size

--- apps/gurps_world/test/gurps_world/worker_test.exs
+++ apps/gurps_world/test/gurps_world/worker_test.exs
@@ -17,4 +17,42 @@ defmodule GurpsWorldWorkerTest do
     end
   end
 
+  describe "world_type size" do
+    test "is :standard when given {:hostile, 6}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 6)[:size] == :standard)
+    end
+    test "is :tiny when given {:hostile, 7}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 7)[:size] == :tiny)
+    end
+    test "is :tiny when given {:hostile, 9}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 9)[:size] == :tiny)
+    end
+    test "is :standard when given {:hostile, 10}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 10)[:size] == :standard)
+    end
+    test "is :standard when given {:hostile, 12}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 12)[:size] == :standard)
+    end
+    test "is :large when given {:hostile, 13}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 12)[:size] == :large)
+    end
+
+    test "is :standard when given {:barren, 13}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 13)[:size] == :standard)
+    end
+    test "is :standard when given {:barren, 16}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 16)[:size] == :standard)
+    end
+    test "is :large when given {:barren, 17}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 17)[:size] == :large)
+    end
+
+    test "is :standard when given {:garden, 16}" do
+      assert(GurpsWorld.Worker.world_type(:garden, 16)[:size] == :standard)
+    end
+    test "is :large when given {:garden, 17}" do
+      assert(GurpsWorld.Worker.world_type(:garden, 16)[:size] == :large)
+    end
+  end
+
 end
