ce9e7305219aa3a3ca172ac41975b796ca1a6acc
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-09-03 16:14:51 -0700

    Fix general make tests

--- apps/mindjammer_world/test/mindjammer_world_test.exs
+++ apps/mindjammer_world/test/mindjammer_world_test.exs
@@ -2,7 +2,16 @@ defmodule MindjammerWorldTest do
   use ExUnit.Case
   doctest MindjammerWorld
 
-  test "greets the world" do
-    assert MindjammerWorld.hello() == :world
+  test "make(:lost_colony) returns a map" do
+    assert(is_map(MindjammerWorld.make(:lost_colony)))
+  end
+  test "make(:rediscovered) returns a map" do
+    assert(is_map(MindjammerWorld.make(:rediscovered)))
+  end
+  test "make(:commonality) returns a map" do
+    assert(is_map(MindjammerWorld.make(:commonality)))
+  end
+  test "make(:core) returns a map" do
+    assert(is_map(MindjammerWorld.make(:core)))
   end
 end
