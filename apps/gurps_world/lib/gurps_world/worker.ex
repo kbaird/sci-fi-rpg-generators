7c88b40dd52221bbc3d8b61f8f8ab8432ff9fc17
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-09-03 13:23:35 -0700

    Implement world sub-type

--- apps/gurps_world/lib/gurps_world/worker.ex
+++ apps/gurps_world/lib/gurps_world/worker.ex
@@ -42,17 +42,35 @@ defmodule GurpsWorld.Worker do
   defp do_overall_type(roll) when roll < 14, do: :barren
   defp do_overall_type(_roll),               do: :garden
 
-  defp do_world_type(:hostile, roll) when roll < 7,  do: %{size: :standard}
-  defp do_world_type(:hostile, roll) when roll < 10, do: %{size: :tiny}
-  defp do_world_type(:hostile, roll) when roll < 13, do: %{size: :standard}
-  defp do_world_type(:hostile, _roll), do: %{size: :large}
+  defp do_world_type(overall_type, roll) do
+    %{size:     world_size(overall_type, roll),
+      sub_type: world_subtype(overall_type, roll)}
+  end
 
-  defp do_world_type(:barren, roll) when roll < 7,  do: %{size: :small}
-  defp do_world_type(:barren, roll) when roll < 11, do: %{size: :tiny}
-  defp do_world_type(:barren, roll) when roll < 13, do: %{size: :asteroid_belt}
-  defp do_world_type(:barren, roll) when roll < 17, do: %{size: :standard}
-  defp do_world_type(:barren, _roll), do: %{size: :large}
+  defp world_size(:hostile, roll) when roll < 7,  do: :standard
+  defp world_size(:hostile, roll) when roll < 10, do: :tiny
+  defp world_size(:hostile, roll) when roll < 13, do: :standard
+  defp world_size(:hostile, _roll), do: :large
 
-  defp do_world_type(:garden, roll) when roll < 17, do: %{size: :standard}
-  defp do_world_type(:garden, _roll), do: %{size: :large}
+  defp world_size(:barren, roll) when roll < 7,  do: :small
+  defp world_size(:barren, roll) when roll < 11, do: :tiny
+  defp world_size(:barren, roll) when roll < 13, do: :asteroid_belt
+  defp world_size(:barren, roll) when roll < 17, do: :standard
+  defp world_size(:barren, _roll), do: :large
+
+  defp world_size(:garden, roll) when roll < 17, do: :standard
+  defp world_size(:garden, _roll), do: :large
+
+  defp world_subtype(:hostile, roll) when roll in [3,4,17,18], do: :cthonian
+  defp world_subtype(:hostile, roll) when roll in [5,6,15,16], do: :greenhouse
+  defp world_subtype(:hostile, roll) when roll < 10, do: :sulfur
+  defp world_subtype(:hostile, roll) when roll < 15, do: :ammonia
+
+  defp world_subtype(:barren, roll) when roll in [3,16], do: :hadean
+  defp world_subtype(:barren, roll) when roll in [4,9,10,15,18], do: :ice
+  defp world_subtype(:barren, roll) when roll < 9,  do: :rock
+  defp world_subtype(:barren, roll) when roll < 13, do: :belt
+  defp world_subtype(:barren, roll) when roll in [13,14,17], do: :ocean
+
+  defp world_subtype(:garden, _roll), do: :garden
 end
--- apps/gurps_world/test/gurps_world/worker_test.exs
+++ apps/gurps_world/test/gurps_world/worker_test.exs
@@ -70,4 +70,66 @@ defmodule GurpsWorldWorkerTest do
     end
   end
 
+  describe "world_type sub_type" do
+    test "is :cthonian when given {:hostile, 4}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 4)[:sub_type] == :cthonian)
+    end
+    test "is :greenhouse when given {:hostile, 6}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 6)[:sub_type] == :greenhouse)
+    end
+    test "is :sulfur when given {:hostile, 9}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 9)[:sub_type] == :sulfur)
+    end
+    test "is :ammonia when given {:hostile, 14}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 14)[:sub_type] == :ammonia)
+    end
+    test "is :greenhouse when given {:hostile, 16}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 16)[:sub_type] == :greenhouse)
+    end
+    test "is :cthonian when given {:hostile, 17}" do
+      assert(GurpsWorld.Worker.world_type(:hostile, 17)[:sub_type] == :cthonian)
+    end
+
+    test "is :hadean when given {:barren, 3}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 3)[:sub_type] == :hadean)
+    end
+    test "is :ice when given {:barren, 4}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 4)[:sub_type] == :ice)
+    end
+    test "is :rock when given {:barren, 8}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 8)[:sub_type] == :rock)
+    end
+    test "is :ice when given {:barren, 10}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 10)[:sub_type] == :ice)
+    end
+    test "is :belt when given {:barren, 11}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 11)[:sub_type] == :belt)
+    end
+    test "is :belt when given {:barren, 12}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 12)[:sub_type] == :belt)
+    end
+    test "is :ocean when given {:barren, 14}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 14)[:sub_type] == :ocean)
+    end
+    test "is :ice when given {:barren, 15}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 15)[:sub_type] == :ice)
+    end
+    test "is :hadean when given {:barren, 16}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 16)[:sub_type] == :hadean)
+    end
+    test "is :ocean when given {:barren, 17}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 17)[:sub_type] == :ocean)
+    end
+    test "is :ice when given {:barren, 18}" do
+      assert(GurpsWorld.Worker.world_type(:barren, 18)[:sub_type] == :ice)
+    end
+
+    test "is :garden when given {:garden, 3}" do
+      assert(GurpsWorld.Worker.world_type(:garden, 3)[:sub_type] == :garden)
+    end
+    test "is :garden when given {:garden, 18}" do
+      assert(GurpsWorld.Worker.world_type(:garden, 18)[:sub_type] == :garden)
+    end
+  end
+
 end
